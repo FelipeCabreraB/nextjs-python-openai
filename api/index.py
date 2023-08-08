75b9e632016aa0c1c53d4c435ef9ef57792282c2
Felipe Cabrera <felipemartincabrera@gmail.com>
2023-08-08 16:07:35 -0300

    Add fastapi cors

--- api/index.py
+++ api/index.py
@@ -1,8 +1,20 @@
 import json
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from api.helpers.openai import revalidate, related, search, chat_query, clear_memory
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Revalidate the Chroma instance
 @app.get("/api/revalidate")
